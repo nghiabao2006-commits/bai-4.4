4a8db85d53bb5cac37db29dc75e0d829cbcec0b4
Admin <Admin@DESKTOP-KMNS09Q>
2026-09-23 15:54:14 +0700

    Add project files.

---
+++ bai 4.4/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace bai_4._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private System.Windows.Forms.GroupBox groupBoxMenu;
+        private System.Windows.Forms.GroupBox groupBoxSelected;
+        private System.Windows.Forms.ListBox lstMenu;
+        private System.Windows.Forms.ListBox lstSelected;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lblTotal;
+
+        private void InitializeComponent()
+        {
+            this.groupBoxMenu = new System.Windows.Forms.GroupBox();
+            this.lstMenu = new System.Windows.Forms.ListBox();
+            this.groupBoxSelected = new System.Windows.Forms.GroupBox();
+            this.lstSelected = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.groupBoxMenu.SuspendLayout();
+            this.groupBoxSelected.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxMenu
+            // 
+            this.groupBoxMenu.Controls.Add(this.lstMenu);
+            this.groupBoxMenu.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxMenu.Name = "groupBoxMenu";
+            this.groupBoxMenu.Size = new System.Drawing.Size(250, 350);
+            this.groupBoxMenu.TabIndex = 0;
+            this.groupBoxMenu.TabStop = false;
+            this.groupBoxMenu.Text = "Menu";
+            // 
+            // lstMenu
+            // 
+            this.lstMenu.FormattingEnabled = true;
+            this.lstMenu.ItemHeight = 16;
+            this.lstMenu.Location = new System.Drawing.Point(6, 21);
+            this.lstMenu.Name = "lstMenu";
+            this.lstMenu.Size = new System.Drawing.Size(238, 308);
+            this.lstMenu.TabIndex = 0;
+            // 
+            // groupBoxSelected
+            // 
+            this.groupBoxSelected.Controls.Add(this.lstSelected);
+            this.groupBoxSelected.Location = new System.Drawing.Point(338, 12);
+            this.groupBoxSelected.Name = "groupBoxSelected";
+            this.groupBoxSelected.Size = new System.Drawing.Size(250, 350);
+            this.groupBoxSelected.TabIndex = 1;
+            this.groupBoxSelected.TabStop = false;
+            this.groupBoxSelected.Text = "Selected";
+            // 
+            // lstSelected
+            // 
+            this.lstSelected.FormattingEnabled = true;
+            this.lstSelected.ItemHeight = 16;
+            this.lstSelected.Location = new System.Drawing.Point(6, 21);
+            this.lstSelected.Name = "lstSelected";
+            this.lstSelected.Size = new System.Drawing.Size(238, 308);
+            this.lstSelected.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(268, 120);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(64, 35);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = ">";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(268, 170);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(64, 35);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "<";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(338, 375);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(58, 16);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "Total: 0k";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 461);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.groupBoxSelected);
+            this.Controls.Add(this.groupBoxMenu);
+            this.Name = "Form1";
+            this.Text = "Fast Food Order";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxMenu.ResumeLayout(false);
+            this.groupBoxSelected.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ bai 4.4/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai_4._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private class MenuItem
+        {
+            public string Name { get; }
+            public int Price { get; }
+
+            public MenuItem(string name, int price)
+            {
+                Name = name;
+                Price = price;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} - {Price}k";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Populate menu
+            lstMenu.Items.Add(new MenuItem("Hamburger", 50));
+            lstMenu.Items.Add(new MenuItem("Pizza", 120));
+            lstMenu.Items.Add(new MenuItem("Gà Rán", 35));
+            lstMenu.Items.Add(new MenuItem("Pepsi", 15));
+
+            // Optional: double-click to add
+            lstMenu.DoubleClick += (s, ev) => btnAdd_Click(s, EventArgs.Empty);
+            lstSelected.DoubleClick += (s, ev) => btnRemove_Click(s, EventArgs.Empty);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (lstMenu.SelectedItem is MenuItem mi)
+            {
+                // add a copy to selected (allow duplicates)
+                lstSelected.Items.Add(new MenuItem(mi.Name, mi.Price));
+                UpdateTotal();
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstSelected.SelectedIndex >= 0)
+            {
+                lstSelected.Items.RemoveAt(lstSelected.SelectedIndex);
+                UpdateTotal();
+            }
+        }
+
+        private void UpdateTotal()
+        {
+            int sum = 0;
+            foreach (var item in lstSelected.Items)
+            {
+                if (item is MenuItem mi)
+                    sum += mi.Price;
+            }
+            lblTotal.Text = $"Total: {sum}k";
+        }
+    }
+}
